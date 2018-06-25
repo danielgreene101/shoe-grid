28e10add05737b1bc919575da17d1ad550f68df5
DanielGreene101 <d.b.greene101@gmail.com>
2018-06-25 16:54:17 -0500

    basic fuctionality

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 import Shoe from './Components/shoes';
 
 class App extends Component {
--- src/Components/shoes.js
+++ src/Components/shoes.js
@@ -8,23 +8,58 @@ class Shoe extends Component {
       style: 'running',
       size: '11',
       upcId: '643596234987',
-      open: false,
+      editing: false,
     };
   }
     editShoe = () => {
-      this.setState({ open: true })
+      this.setState({ editing: !this.state.editing })
     }
+
+    updateStyle = (event) => {
+      console.log('value', event.target.value);
+      this.setState({ style: event.target.value  })
+    }
+    updateBrand = (event) => {
+      console.log('value', event.target.value);
+      this.setState({ brand: event.target.value  })
+    }
+    updateSize = (event) => {
+      console.log('value', event.target.value);
+      this.setState({ size: event.target.value  })
+    }
+    updateUpcId = (event) => {
+      console.log('value', event.target.value);
+      this.setState({ upcId: event.target.value  })
+    }
+
     render() {
+      const { 
+        editing,
+        brand,
+        size,
+        upcId,
+        style
+        } = this.state
 
       return (
         <div style={{border: '1px solid red', display: 'flex', width: 200, height: 200}}>
           <div style={{ display: 'block'}}>
-            <p>Brand: {this.state.brand}</p>
-            <p>Style: {this.state.style}</p>
-            <p>Size: {this.state.size}</p>
-            <p>upcId: {this.state.upcId}</p>
+          {!editing ?
+            <div> 
+              <p>Brand: {brand}</p>
+              <p>Style: {style}</p>
+              <p>Size: {size}</p>
+              <p>upcId: {upcId}</p>
+            </div> :
+            <div>
+              Brand: <input value={this.state.brand} onChange={this.updateBrand}/>
+              Style: <input value={this.state.style} onChange={this.updateStyle}/>
+              Size: <input value={this.state.size} onChange={this.updateSize}/>
+              upcId: <input value={this.state.upcId} onChange={this.updateUpcId}/>
+            </div>
+          }
             <button onClick={this.editShoe} >
-              Edit Shoe
+              {editing ? 'Save Shoe' : 'Edit Shoe'}
             </button>
           </div>
         </div>
